b24d55780599a50cc21ba9cda5137e0796346f15
Fahad Hassan <fahadhassan2499@gmail.com>
2021-09-05 16:31:00 +0530

    Initial change

--- pattern2.c
+++ pattern2.c
@@ -1,27 +1,27 @@
-#include<stdio.h>
-int main(){
-    int k;
-    k=3;
-    int low=1;
-    int high=2*k-1;
-    int mid=(low+high)/2;
-    for(int i=low;i<=k;i++){
-        for(int j=low;j<=mid;j++){
-            if(j==mid || i+j>k){
-                printf("*");
-            }
-            else
-                printf(" ");
-        }
-        for(int l=mid+1;l<=high;l++){
-            if(l-i<k){
-                printf("*");
-            }
-            else
-                printf(" ");
-        }
+// #include<stdio.h>
+// int main(){
+//     int k;
+//     k=3;
+//     int low=1;
+//     int high=2*k-1;
+//     int mid=(low+high)/2;
+//     for(int i=low;i<=k;i++){
+//         for(int j=low;j<=mid;j++){
+//             if(j==mid || i+j>k){
+//                 printf("*");
+//             }
+//             else
+//                 printf(" ");
+//         }
+//         for(int l=mid+1;l<=high;l++){
+//             if(l-i<k){
+//                 printf("*");
+//             }
+//             else
+//                 printf(" ");
+//         }
         
-        printf("\n");
-    }
+//         printf("\n");
+//     }
 
-}
+// }
